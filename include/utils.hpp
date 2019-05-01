ce7bee689ca35a0a2a7dafc2d516d24c69a94798
Kim Gyeongsik <admin@gyungdal.cc>
2019-05-01 23:45:15 +0900

    utils update

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,7 +3,12 @@
 
 // x65599 hashing algorithm
 unsigned long long Hash(const char* str){
-    return str[0] ? (str[0] + 0xEDB8832Full * Hash(str + 1)) : 0x8603Full;
+    unsigned long long hash = 0;
+    while (*str){
+        hash = 65599 * hash + str[0];
+        str++;
+    }
+    return hash ^ (hash >> 16);
 }
 
 #endif
