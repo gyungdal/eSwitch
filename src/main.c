46e554b568cf2d27faaca68a781ccf593e417df6
Kim Gyeongsik <admin@gyungdal.cc>
2019-05-02 21:40:43 +0900

    pc status checker 제거

--- src/main.c
+++ src/main.c
@@ -165,109 +165,6 @@ static void http_server(void *pvParameters) {
     netconn_delete(conn);
 }
 
-
-static void receive_pc_status(void *param) {
-    #define WEB_SERVER "nas.gyungdal.cc"
-    #define WEB_PORT 443
-    #define WEB_URL "https://nas.gyungdal.cc/"
-
-    static const char *REQUEST = "GET " WEB_URL " HTTP/1.0\r\n"
-                                "Host: " WEB_SERVER "\r\n"
-                                "User-Agent: esp-idf/1.0 esp32\r\n"
-                                "\r\n";
-    const struct addrinfo hints = {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_STREAM,
-    };
-    struct addrinfo *res;
-    struct in_addr *addr;
-    int s, r;
-    char recv_buf[64];
-    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
-                        false, true, portMAX_DELAY);
-    ESP_LOGI(TAG, "[INFO] WiFi Check!");
-
-    int err = getaddrinfo(WEB_SERVER, "80", &hints, &res);
-
-    if (err != 0 || res == NULL)
-    {
-        ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        return;
-    }
-
-    /* Code to print the resolved IP.
-        Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
-    addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-    ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
-
-    s = socket(res->ai_family, res->ai_socktype, 0);
-    if (s < 0)
-    {
-        ESP_LOGE(TAG, "... Failed to allocate socket.");
-        freeaddrinfo(res);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        return;
-    }
-    ESP_LOGI(TAG, "... allocated socket");
-
-    if (connect(s, res->ai_addr, res->ai_addrlen) != 0)
-    {
-        ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
-        close(s);
-        freeaddrinfo(res);
-        vTaskDelay(4000 / portTICK_PERIOD_MS);
-        return;
-    }
-
-    ESP_LOGI(TAG, "... connected");
-    freeaddrinfo(res);
-
-    if (write(s, REQUEST, strlen(REQUEST)) < 0)
-    {
-        ESP_LOGE(TAG, "... socket send failed");
-        close(s);
-        vTaskDelay(4000 / portTICK_PERIOD_MS);
-        return;
-    }
-    ESP_LOGI(TAG, "... socket send success");
-
-    struct timeval receiving_timeout;
-    receiving_timeout.tv_sec = 5;
-    receiving_timeout.tv_usec = 0;
-    if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                    sizeof(receiving_timeout)) < 0)
-    {
-        ESP_LOGE(TAG, "... failed to set socket receiving timeout");
-        close(s);
-        vTaskDelay(4000 / portTICK_PERIOD_MS);
-        return;
-    }
-    ESP_LOGI(TAG, "... set socket receiving timeout success");
-
-    /* Read HTTP response */
-    do
-    {
-        bzero(recv_buf, sizeof(recv_buf));
-        r = read(s, recv_buf, sizeof(recv_buf) - 1);
-        for (int i = 0; i < r; i++)
-        {
-            putchar(recv_buf[i]);
-        }
-    } while (r > 0);
-
-    ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
-    close(s);
-    for (int countdown = 10; countdown >= 0; countdown--)
-    {
-        ESP_LOGI(TAG, "%d... ", countdown);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    ESP_LOGI(TAG, "Starting again!");
-    
-
-}
-
 void app_main() {
     ESP_ERROR_CHECK(nvs_flash_init());
     initialise_wifi();
@@ -278,5 +175,4 @@ void app_main() {
         gpio_set_level(GPIO_PINS[i], HIGH);
     }
     xTaskCreate(&http_server, "http_server", 4096, NULL, 1, NULL);
-    xTaskCreate(&receive_pc_status, "receive_pc_status", 2048, NULL, 1, NULL);
 }
